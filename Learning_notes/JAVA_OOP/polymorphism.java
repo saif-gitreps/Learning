08d4103a6583f70b95cf15c7adc900ddb1032569
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-03 01:06:21 +0600

    Create polymorphism.java

---
+++ Learning_notes/JAVA_OOP/polymorphism.java
@@ -0,0 +1,33 @@
+/*
+so polymorphism concept is one function that will be overwritten according to the parameters we provide,
+
+Function Overloading: It is a feature in C++ where multiple functions can have
+the same name but with different parameter lists. The compiler will decide which function to call based
+on the number and types of arguments passed to the function.
+
+Operator Overloading: It is a feature in C++ where the operators such as +, -, * etc.
+can be given additional meanings when applied to user-defined data types.
+
+*/
+
+package polymorph;
+//this is a simple example for compile polymorphism.
+public class Person {
+
+    public void eat(int a){
+        System.out.println("this is an integer");
+    }
+    public void eat(String a){
+        System.out.println("This is a string");
+    }
+    public void eat(float a){
+        System.out.println("this is a float");
+    }
+    public static void main(String args[]){
+        Person saif = new Person();
+        saif.eat(4);
+        saif.eat("hello");
+        saif.eat(5.6F);
+    }
+}
+// this is an example of run time polymorphism 
