e523913fc4e406d43ee5f920abf60d83f322d108
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-16 00:05:04 +0600

    Add files via upload

---
+++ Learning_notes/JAVA_OOP/practise1/Material.java
@@ -0,0 +1,10 @@
+package prctice1.try1;
+
+public class Material {
+    String topicName;
+    String topicURL;
+    public Material(String topicName,String topicURL){
+        topicName = this.topicName;
+        topicURL = this.topicURL;
+    }
+}
