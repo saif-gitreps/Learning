057a658356c78b4c2318d67cf3b7d7793b8242d4
Saif <droconos99@gmail.com>
2023-06-30 18:30:33 +0600

    added recursion study of generating unqie subset

--- Learning_notes/recursion-study/Generate_unique_subset.cpp
+++ Learning_notes/recursion-study/Generate_unique_subset.cpp
@@ -1,12 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define newline() "\n"
-#define enter_arr(x,n) for(int i=0;i<n;i++)cin>>x[i]
-#define out_arr(x,n) for(int i=0;i<n;i++)cout<<x[i]<<" "
-#define debug(a) cerr<< "line "<<__LINE__ <<" : "<< #a <<" --> "<<(a)<<"\n"
-#define GGG 9223372036854775807
-#define pb push_back
 #define FAST(SUIII); ios_base::sync_with_stdio(false);cin.tie(NULL);
 const int MAXN = 100010;
 
