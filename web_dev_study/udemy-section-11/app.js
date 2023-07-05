a64efdf5307a5f47ac370f45012ce72deeb58cde
Saif <droconos99@gmail.com>
2023-07-05 21:17:24 +0600

    practising JS

--- web_dev_study/udemy-section-11/app.js
+++ web_dev_study/udemy-section-11/app.js
@@ -1,15 +1,9 @@
+let linked_list = {
+  data: null,
+  next: linked_list,
+};
+
 function int_main() {
-  let user1 = {
-    name: "saif",
-    age: 34,
-  };
-  let user2 = {
-    name: "Sara",
-    age: 10,
-  };
-  let a = [user1, user2];
-  a[0].name = "kaif";
-  console.log(a[0].name);
   return 0;
 }
 
