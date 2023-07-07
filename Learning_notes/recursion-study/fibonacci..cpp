d2984943bcc58aae7e88bb0bdd77b347a6e7cb0b
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-07 23:19:25 +0600

    Update fibonacci..cpp

--- Learning_notes/recursion-study/fibonacci..cpp
+++ Learning_notes/recursion-study/fibonacci..cpp
@@ -3,10 +3,6 @@ using namespace std;
 #define nexline() std::cout<<newline()
 
 const int MAXN = 100010;
-vector<int> find_factors(int n){vector<int> f;
-for (int x=2;x*x<=n;x++)
-{while(n%x==0) {f.push_back(x);n /= x;} }
-if (n > 1) f.push_back(n);return f; }
 /*==================================================*/
 
 int rev(int n){
