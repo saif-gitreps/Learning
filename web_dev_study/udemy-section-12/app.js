ef41a642c02c679cb7e50070f2a0c6e67f827592
Saif <droconos99@gmail.com>
2023-07-16 18:15:47 +0600

    continuing section-12

--- web_dev_study/udemy-section-12/app.js
+++
@@ -1,8 +0,0 @@
-let header1 = document.body.children[0];
-header1.parentElement.style.backgroundColor = "gray";
-console.log(header1.nextElementSibling.compareDocumentPosition);
-
-let new_variable_for_head = document.getElementById("myHeader");
-
-let selecting_para = document.querySelector("p");
-selecting_para.textContent = "saifrrrr";
