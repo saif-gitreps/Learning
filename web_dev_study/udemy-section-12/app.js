4d66d9cae2a08a87defa5167b822b87617b67707
Saif <droconos99@gmail.com>
2023-07-16 19:07:20 +0600

    studying how to add/remove/move elements by js

---
+++ web_dev_study/udemy-section-12/app.js
@@ -0,0 +1,10 @@
+// adding an element
+let newAnchorElement = document.createElement("a");
+
+let firstPara = document.querySelector("p");
+
+firstPara.append(newAnchorElement);
+firstPara.inse;
+newAnchorElement.textContent = " new link doe";
+
+let firstH1Element = document.querySelector("h1");
