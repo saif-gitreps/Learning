d3d6b4f0a8837691419216114e018893bda5299a
Saif <droconos99@gmail.com>
2023-07-18 23:04:14 +0600

    coudnt figure out many of the things that are needed to know

--- web_dev_study/udemy-section-12/app.js
+++ web_dev_study/udemy-section-12/app.js
@@ -2,8 +2,14 @@
 //using namespace std;
 //-----------------------------------------------//
 
+function checkLength(event) {
+  let enterText = textField.event.target.input;
+  let len = enterText.length;
+  counter.textContent = len + "/60";
+}
+
 //---------------------MAIN----------------------//
 
 let textField = document.querySelector("input");
 let counter = document.body.children[0].children[2];
-console.dir(counter);
+textField.addEventListener("input", checkLength);
