0aeab0dafecc65b2830117578ec079d0c46fd7e4
Saif <droconos99@gmail.com>
2023-07-21 14:43:51 +0600

    did my part of the exercise

--- web_dev_study/udemy-section-12/practise/exercise-2/exercise.js
+++ web_dev_study/udemy-section-12/practise/exercise-2/exercise.js
@@ -9,22 +9,26 @@ let second_button = document.getElementById("second-button");
 //    The functions should "console.dir()" the clicked buttons.
 //    - Output the first button by using the variable in which it's stored
 //    - Output the second button WITHOUT using the variable in which it's stored
-function first_button_function(event) {
-  console.dir(first_button);
-}
-function second_button_function() {
-  console.dir(second_button);
-}
-first_button.addEventListener("click", first_button_function);
-second_button.addEventListener("click", second_button_function);
 
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
 //    (first and third paragraph)
 //    - Select BOTH paragraphs by drilling into the document and "navigating" to the
 //      mentioned elements
 //    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
+let first_para = document.body.children[2].children[3];
+let second_para = document.body.children[2].children[5];
+
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
 //    - The second button changes the background color of the first paragraph to blue
+first_button.addEventListener("click", first_button_function);
+second_button.addEventListener("click", second_button_function);
+function first_button_function() {
+  first_para.parentElement.removeChild(first_para);
+}
+function second_button_function() {
+  first_para.classList.add("bg-color");
+}
+
 // 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
 //    Note: You'll have to add those classes to the styles.css file first!
