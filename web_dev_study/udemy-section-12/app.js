dad1bd51e6ff8f6bf84e590597c350b53ef43399
Saif <droconos99@gmail.com>
2023-07-27 17:45:38 +0600

    compelted the demo project the way i wanted it to be done

--- web_dev_study/udemy-section-12/app.js
+++ web_dev_study/udemy-section-12/app.js
@@ -6,12 +6,20 @@
 function checkLength(event) {
   let enteredText = event.target.value;
   let enteredTextLen = maxLengthOfTextField - enteredText.length;
-  if (enteredTextLen <= 10) {
-    textField.style.backgroundColor = "#ffcccb";
-  } else {
-    textField.style.backgroundColor = "white";
-  }
   counter.textContent = enteredTextLen;
+  if (enteredTextLen <= 10) {
+    textField.classList.add("warning");
+    counter.classList.add("warning");
+  } else {
+    textField.classList.remove("warning");
+    counter.classList.remove("warning");
+  }
+}
+function resetAction(event) {
+  counter.textContent = "60";
+  textField.value = "";
+  counter.classList.remove("warning");
+  textField.classList.remove("warning");
 }
 //---------------------MAIN----------------------//
 const textField = document.getElementById("Product-name");
@@ -19,3 +27,4 @@ const counter = document.getElementById("remaining-counter");
 const maxLengthOfTextField = textField.maxLength;
 const clearButton = document.querySelector("button");
 textField.addEventListener("input", checkLength);
+clearButton.addEventListener("click", resetAction);
