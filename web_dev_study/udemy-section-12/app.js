cac187f1141298a59a4706176315461c903beb3f
Saif <droconos99@gmail.com>
2023-07-27 18:09:34 +0600

    adding finishes touches to demo

--- web_dev_study/udemy-section-12/app.js
+++ web_dev_study/udemy-section-12/app.js
@@ -7,9 +7,12 @@ function checkLength(event) {
   let enteredText = event.target.value;
   let enteredTextLen = maxLengthOfTextField - enteredText.length;
   counter.textContent = enteredTextLen;
-  if (enteredTextLen <= 10) {
-    textField.classList.add("warning");
+  if (enteredTextLen === 0) {
     counter.classList.add("warning");
+    textField.classList.add("error");
+  } else if (enteredTextLen <= 10) {
+    counter.classList.add("warning");
+    textField.classList.add("warning");
   } else {
     textField.classList.remove("warning");
     counter.classList.remove("warning");
@@ -20,6 +23,7 @@ function resetAction(event) {
   textField.value = "";
   counter.classList.remove("warning");
   textField.classList.remove("warning");
+  textField.classList.remove("error");
 }
 //---------------------MAIN----------------------//
 const textField = document.getElementById("Product-name");
