fa0379b97c8a204659c94a6932f67e2e4824751d
Saif <droconos99@gmail.com>
2023-07-28 11:24:48 +0600

    finished problem 1 from demo of section 13

--- web_dev_study/udemy-section-13/09-practice-time-starting-setup/loops-in-action/loops-in-js.js
+++ web_dev_study/udemy-section-13/09-practice-time-starting-setup/loops-in-action/loops-in-js.js
@@ -1,6 +1,13 @@
 // code: sum numbers
 const calculateSumButton = document.querySelector("#calculator button");
 
-function calculateSum(event) {}
+function calculateSum(event) {
+  const userNumberInput = document.getElementById("user-number");
+  const enteredNumber = parseInt(userNumberInput.value);
+  const outPutField = document.getElementById("calculated-sum");
+  let ans = enteredNumber * ((enteredNumber - 1) / 2) + enteredNumber;
+  outPutField.textContent = ans;
+  outPutField.style.display = "block";
+}
 
 calculateSumButton.addEventListener("click", calculateSum);
