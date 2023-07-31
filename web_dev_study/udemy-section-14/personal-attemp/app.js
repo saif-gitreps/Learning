80cf9456656dbb8c5317b1973037799d33184583
Saif <droconos99@gmail.com>
2023-07-31 19:06:39 +0600

    adding javascript logic to the tak game

---
+++ web_dev_study/udemy-section-14/personal-attemp/app.js
@@ -0,0 +1,5 @@
+const editPlayer1Button = document.getElementById("edit-player-1");
+const editPlayer2Button = document.getElementById("edit-player-2");
+
+editPlayer1Button.addEventListener("click", openPLayerConfig);
+editPlayer2Button.addEventListener("click", openPLayerConfig);
