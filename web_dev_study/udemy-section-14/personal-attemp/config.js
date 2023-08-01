0286bbdd8b5ba5725f8cb2df995bb14cd5637bf1
Saif <droconos99@gmail.com>
2023-08-01 20:06:05 +0600

    completed overlay

--- web_dev_study/udemy-section-14/personal-attemp/config.js
+++ web_dev_study/udemy-section-14/personal-attemp/config.js
@@ -5,10 +5,19 @@ function openPLayerConfig(event) {
 function closePlayerConfig(event) {
   playerConfigOverlay.style.display = "none";
   backdrop.style.display = "none";
+  form.firstElementChild.classList.remove("error");
+  errorOutputPara.textContent = "";
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
+  event.target.firstElementChild.classList.remove("error");
+  errorOutputPara.textContent = "";
   const enteredPlayerName = formData.get("playername").trim();
+  if (enteredPlayerName == "") {
+    event.target.firstElementChild.classList.add("error");
+    errorOutputPara.textContent = "Please enter a valid name";
+    return;
+  }
 }
