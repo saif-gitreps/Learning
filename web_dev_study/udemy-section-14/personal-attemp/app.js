59e23d2a3994dc2b32af8d981b304ceabc613830
Saif <droconos99@gmail.com>
2023-08-03 22:58:20 +0600

    starting to add logic to the boards

--- web_dev_study/udemy-section-14/personal-attemp/app.js
+++ web_dev_study/udemy-section-14/personal-attemp/app.js
@@ -14,12 +14,17 @@ const playerConfigOverlay = document.getElementById("config-overlay");
 const backdrop = document.getElementById("backdrop");
 const form = document.querySelector("form");
 const errorOutputPara = document.getElementById("config-error");
+const activeGameSection = document.getElementById("active-game");
+
 const editPlayer1Button = document.getElementById("edit-player-1");
 const editPlayer2Button = document.getElementById("edit-player-2");
 const closePlayerConfigButton = document.getElementById("cancel-button");
+const startNewGameButton = document.getElementById("start-game-button");
 
 editPlayer1Button.addEventListener("click", openPLayerConfig);
 editPlayer2Button.addEventListener("click", openPLayerConfig);
 
 closePlayerConfigButton.addEventListener("click", closePlayerConfig);
 form.addEventListener("submit", savePlayerConfig);
+
+startNewGameButton.addEventListener("click", startNewGameButton);
