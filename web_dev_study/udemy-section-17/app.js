7cbbc7138066a6a2053836aa5c25436b1d840ff2
Saif <droconos99@gmail.com>
2023-09-04 17:06:09 +0600

    successfully created a simple server

--- web_dev_study/udemy-section-17/app.js
+++ web_dev_study/udemy-section-17/app.js
@@ -1,5 +1,11 @@
 const http = require("http");
 
+function handleRequest(request, respose) {
+   response.statusCode = 200;
+   //404 means url not found
+   response.end("<h1> Hellow World </h1>");
+}
+
 const server = http.createServer();
 
 server.listen(3000);
