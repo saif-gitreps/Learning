ea4c2afecddb8383dc327b63d5c387260b712caa
Saif <droconos99@gmail.com>
2023-09-05 20:51:53 +0600

    accepting user input from express server

--- web_dev_study/udemy-section-18/app.js
+++ web_dev_study/udemy-section-18/app.js
@@ -6,8 +6,10 @@ app.get("/currenttime", (req, res, next) => {
    res.send("<h1>" + new Date().toISOString() + "</h1>");
 }); // send request to localhost:3000/currenttime.
 
-app.get("/", (req, res, next) => {
-   res.send("<h1> hello </h1>");
+app.get("/", (req, res) => {
+   res.send(
+      "<form action='/store-user' method='POST'> <input type='text'> <button> Submit </button> </form>"
+   );
 }); // send request ot locahost:3000.
 
 app.listen(3000);
