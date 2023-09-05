761ac4c10efcb10a1ca6494679ca5019e5c29861
Saif <droconos99@gmail.com>
2023-09-05 16:49:47 +0600

    starting section-18 workign with express

---
+++ web_dev_study/udemy-section-18/app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+function handleRequest(request, response) {
+   if (request.url == "/currenttime") {
+      response.statusCode = 200;
+      response.end("<h1>" + new Date().toISOString() + "</h1>");
+   } else if (request.url == "/") {
+      response.statusCode = 200;
+      response.end("<h1> Hellow World </h1>");
+   }
+}
+
+const server = http.createServer(handleRequest);
+
+function serverMessage() {
+   console.log("hello");
+}
+
+server.listen(3000, serverMessage);
