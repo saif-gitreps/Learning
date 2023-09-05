92ba7ba16baf8748cb17e44fc76f8266d7e45a76
Saif <droconos99@gmail.com>
2023-09-05 23:26:07 +0600

    parsed incoming request data using express.urlencoder()

--- web_dev_study/udemy-section-18/app.js
+++ web_dev_study/udemy-section-18/app.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/currenttime", (req, res, next) => {
    res.send("<h1>" + new Date().toISOString() + "</h1>");
 }); // send request to localhost:3000/currenttime.
 
 app.get("/", (req, res) => {
    res.send(
-      "<form action='/store-user' method='POST'> <input type='text'> <button> Submit </button> </form>"
+      "<form action='/store-user' method='POST'> <input type='text' name='username'> <button> Submit </button> </form>"
    );
 }); // send request ot locahost:3000.
 
-app.post("/store-user", (request, response) => {});
+app.post("/store-user", (request, response) => {
+   const userName = request.body.username;
+   response.send("<h1>" + userName + "</h1>");
+});
 
 app.listen(3000);
