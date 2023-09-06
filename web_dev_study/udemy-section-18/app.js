5ba948b277a0f6fdc80449007e7c32c07511f1a4
Saif <droconos99@gmail.com>
2023-09-06 23:48:37 +0600

    added comments inbetween new concepts

--- web_dev_study/udemy-section-18/app.js
+++ web_dev_study/udemy-section-18/app.js
@@ -1,10 +1,15 @@
+// importing file system package.
 const fs = require("fs");
+
+// importing path package to make directory path easier.
 const path = require("path");
 
+// importing express.js
 const express = require("express");
 
 const app = express();
 
+// parsing every request data (JSON) to readable js code.
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/currenttime", (req, res, next) => {
@@ -18,15 +23,19 @@ app.get("/", (req, res) => {
 }); // send request ot locahost:3000.
 
 app.post("/store-user", (request, response) => {
+   // holds the data that was sent.
    const userName = request.body.username;
 
+   // making a global path for the text file
    const filePath = path.join(__dirname, "data", "users.json");
 
    const fileData = fs.readFileSync(filePath);
    const existingUsers = JSON.parse(fileData);
 
+   // adding new value as in array to the json textfile (now it is in array).
    existingUsers.push(userName);
 
+   // converting to .json format
    fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
    response.send("<h1>" + userName + "</h1>");
