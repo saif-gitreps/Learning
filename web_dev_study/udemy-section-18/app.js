15f382fbf8c869c1ce40a7a2cbb7b5cc31822f9f
Saif <droconos99@gmail.com>
2023-09-06 18:51:46 +0600

    i am a smooth operatoooor

--- web_dev_study/udemy-section-18/app.js
+++ web_dev_study/udemy-section-18/app.js
@@ -4,19 +4,17 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/currenttime", (req, res, next) => {
-   res.send("<h1>" + new Date().toISOString() + "</h1>");
-}); // send request to localhost:3000/currenttime.
-
-app.get("/", (req, res) => {
-   res.send(
-      "<form action='/store-user' method='POST'> <input type='text' name='username'> <button> Submit </button> </form>"
+app.get("/", (request, response) => {
+   response.send(
+      "<form action = '/user' method = 'POST'> <input name = 'username'> <button>click here buddy</button> </form>"
    );
-}); // send request ot locahost:3000.
-
-app.post("/store-user", (request, response) => {
-   const userName = request.body.username;
-   response.send("<h1>" + userName + "</h1>");
 });
 
-app.listen(3000);
+app.post("/user", (request, response) => {
+   let userName = request.body.username;
+   response.send("<p>" + userName + "</p>");
+});
+
+app.listen(3000, () => {
+   console.log("im listening , smooooooooooth operatorrrrrrr");
+});
