348926ae0664f49e1cade1737b0993d75dd32297
Saif <droconos99@gmail.com>
2023-09-06 19:08:18 +0600

    adding path and fs package into our code system

--- web_dev_study/udemy-section-18/app.js
+++ web_dev_study/udemy-section-18/app.js
@@ -1,20 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (request, response) => {
-   response.send(
-      "<form action = '/user' method = 'POST'> <input name = 'username'> <button>click here buddy</button> </form>"
+app.get("/currenttime", (req, res, next) => {
+   res.send("<h1>" + new Date().toISOString() + "</h1>");
+}); // send request to localhost:3000/currenttime.
+
+app.get("/", (req, res) => {
+   res.send(
+      "<form action='/store-user' method='POST'> <input type='text' name='username'> <button> Submit </button> </form>"
    );
+}); // send request ot locahost:3000.
+
+app.post("/store-user", (request, response) => {
+   const userName = request.body.username;
+
+   fs.writeFileSync("");
+
+   response.send("<h1>" + userName + "</h1>");
 });
 
-app.post("/user", (request, response) => {
-   let userName = request.body.username;
-   response.send("<p>" + userName + "</p>");
-});
-
-app.listen(3000, () => {
-   console.log("im listening , smooooooooooth operatorrrrrrr");
-});
+app.listen(3000);
