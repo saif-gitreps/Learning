3dddeca3b9dbca69bf7c3e31be93a62b934245f7
Saif <droconos99@gmail.com>
2023-09-07 23:48:33 +0600

    added route and responses to 6 different html files

--- web_dev_study/udemy-section-19/more/code/00-starting-project/app.js
+++ web_dev_study/udemy-section-19/more/code/00-starting-project/app.js
@@ -6,11 +6,33 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {});
+app.get("/", (request, response) => {
+   response.send(" <h1> hi </h1> ");
+});
 
 app.get("/restaurants", (request, response) => {
    const restaurantsHtml = path.join(__dirname, "views", "restaurants.html");
    response.sendFile(restaurantsHtml);
+});
+
+app.get("/recommend", (request, response) => {
+   const recommendHtml = path.join(__dirname, "views", "recommend.html");
+   response.sendFile(recommendHtml);
+});
+
+app.get("/home", (request, response) => {
+   const frontPage = path.join(__dirname, "views", "index.html");
+   response.send(frontPage);
+});
+
+app.get("/confirm", (request, response) => {
+   const confirmHtml = path.join(__dirname, "views", "confirm.html");
+   response.send(confirmHtml);
+});
+
+app.get("/about", (request, response) => {
+   const aboutPage = path.join(__dirname, "views", "about.html");
+   response.send(aboutPage);
 });
 
 app.listen(3000);
