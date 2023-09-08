775ad9bb198def21e813e528e1e2447ee8ba4bd2
Saif <droconos99@gmail.com>
2023-09-08 23:33:46 +0600

    setting up ejs in app.js

--- web_dev_study/udemy-section-19/more/code/00-starting-project/app.js
+++ web_dev_study/udemy-section-19/more/code/00-starting-project/app.js
@@ -6,6 +6,10 @@ const express = require("express");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
