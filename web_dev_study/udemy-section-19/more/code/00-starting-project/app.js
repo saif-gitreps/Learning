481ba633953d15f6319d8119780ea07ad948cc53
Saif <droconos99@gmail.com>
2023-09-10 00:48:41 +0600

    Changed things like converted .html to .ejs and added dynamic ejs syntax to .ejs files

--- web_dev_study/udemy-section-19/more/code/00-starting-project/app.js
+++ web_dev_study/udemy-section-19/more/code/00-starting-project/app.js
@@ -15,17 +15,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
-   const frontPage = path.join(__dirname, "views", "index.html");
-   response.sendFile(frontPage);
+   response.render("index");
 });
 app.get("/restaurants", (request, response) => {
-   const restaurantsHtml = path.join(__dirname, "views", "restaurants.html");
-   response.sendFile(restaurantsHtml);
+   const filePath = path.join(__dirname, "data", "restaurants.json");
+   const fileData = fs.readFileSync(filePath);
+   const storedRestData = JSON.parse(fileData);
+   response.render("restaurants", { numberOfRestaurant: storedRestData.length });
 });
 
 app.get("/recommend", (request, response) => {
-   const recommendHtml = path.join(__dirname, "views", "recommend.html");
-   response.sendFile(recommendHtml);
+   response.render("recommend");
 });
 
 app.post("/recommend", (request, response) => {
@@ -43,13 +43,11 @@ app.post("/recommend", (request, response) => {
 });
 
 app.get("/confirm", (request, response) => {
-   const confirmHtml = path.join(__dirname, "views", "confirm.html");
-   response.sendFile(confirmHtml);
+   response.render("confirm");
 });
 
 app.get("/about", (request, response) => {
-   const aboutPage = path.join(__dirname, "views", "about.html");
-   response.sendFile(aboutPage);
+   response.render("about");
 });
 
 app.listen(3000);
