013ca170069cb646ea16865c048546df27373cd5
Saif <droconos99@gmail.com>
2023-09-13 23:26:41 +0600

    starting new section-20, dynamic roting and custom middlware

---
+++ web_dev_study/udemy-section-20/00-starting-project/app.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+   response.render("index");
+});
+app.get("/restaurants", (request, response) => {
+   const filePath = path.join(__dirname, "data", "restaurants.json");
+   const fileData = fs.readFileSync(filePath);
+   const storedRestData = JSON.parse(fileData);
+   response.render("restaurants", { numberOfRestaurant: storedRestData.length, restaurants: storedRestData });
+});
+
+app.get("/recommend", (request, response) => {
+   response.render("recommend");
+});
+
+app.post("/recommend", (request, response) => {
+   const restaurant = request.body; // this probably returns an object.
+
+   const filePath = path.join(__dirname, "data", "restaurants.json");
+   const fileData = fs.readFileSync(filePath);
+   const storedRestData = JSON.parse(fileData);
+
+   storedRestData.push(restaurant);
+
+   fs.writeFileSync(filePath, JSON.stringify(storedRestData));
+
+   response.redirect("/confirm");
+});
+
+app.get("/confirm", (request, response) => {
+   response.render("confirm");
+});
+
+app.get("/about", (request, response) => {
+   response.render("about");
+});
+
+app.listen(3000);
