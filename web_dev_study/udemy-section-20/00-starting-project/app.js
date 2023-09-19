d65855962d285ff5214fc83b7d4d93ce047daa82
Saif <droconos99@gmail.com>
2023-09-19 20:48:49 +0600

    Simple error response added

--- web_dev_study/udemy-section-20/00-starting-project/app.js
+++ web_dev_study/udemy-section-20/00-starting-project/app.js
@@ -21,7 +21,6 @@ app.get("/restaurants", (request, response) => {
 
 app.get("/restaurants/:id", (request, response) => {
    // step 2 : The id that was accessd from the JSON FILE and sent through ejs file in the form of sub-route.
-
    const filePath = path.join(__dirname, "data", "restaurants.json");
    const fileData = fs.readFileSync(filePath);
    const storedRestData = JSON.parse(fileData);
@@ -29,14 +28,14 @@ app.get("/restaurants/:id", (request, response) => {
    // step 3 : The id is then parse or translated using a param object into a JSON property.
    const restaurantId = request.params.id;
 
-   // step 4 : Now according to the html logic of the subroute , we match using id or the selected card and send the entire object from the arrays of object [if youre confused just reread and understand it], {next-step- -> restaurant=detail}.
-   //[note 'item' is the variable name that will be in the restauarent-details and we are passing an iterator from arrays of objects]
+   // step 4 : Now according to the html logic of the subroute , we match using id or the selected card and send the entire object from the arrays of object [if youre confused just reread and understand it], {next-step- -> restaurant=detail}. [note 'item' is the variable name that will be in the restauarent-details and we are passing an iterator from arrays of objects]
    for (const restaurant of storedRestData) {
       if (restaurant.id == restaurantId) {
          response.render("restaurant-detail", { item: restaurant });
          return;
       }
    }
+   response.send("<h1>Eror 404 not found</h1>");
 });
 
 app.get("/recommend", (request, response) => {
@@ -44,9 +43,11 @@ app.get("/recommend", (request, response) => {
 });
 
 app.post("/recommend", (request, response) => {
-   const restaurant = request.body; // this probably returns an object.
+   const restaurant = request.body;
 
-   restaurant.id = uuid.v4(); // v4() method will give randomly generate unqiue id, in string. A new property in the newly submitted form in the form of 'id' will be added .
+   restaurant.id = uuid.v4();
+   // v4() method will give randomly generate unqiue id, in string.
+   // A new property in the newly submitted form in the form of 'id' will be added .
    const filePath = path.join(__dirname, "data", "restaurants.json");
    const fileData = fs.readFileSync(filePath);
    const storedRestData = JSON.parse(fileData);
