e46a6b62c85f9ffabc2ce016bc441fdb1292040e
Saif <droconos99@gmail.com>
2023-09-27 19:23:34 +0600

    testing rest parameters

--- web_dev_study/udemy-section-21/app.js
+++ web_dev_study/udemy-section-21/app.js
@@ -1,5 +1,14 @@
 function hello(userName = "user") {
    console.log("hi " + userName);
 }
-
 hello();
+
+function sum(...number) {
+   let res = 0;
+   for (let i = 0; i < number.length; i++) {
+      res += number[i];
+   }
+   return res;
+}
+
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
