3685773dd8d5b7c0d0c45f3ee4a2ec88ad8f08b2
Saif <droconos99@gmail.com>
2023-10-07 22:30:52 +0600

    reviewing promises in js

--- web_dev_study/udemy-section-21/async.js
+++ web_dev_study/udemy-section-21/async.js
@@ -1,11 +1,20 @@
-const fs = require("fs");
+// one way-> const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
    let fileData;
-   fs.readFile("data.txt", function (error, fileData) {
-      console.log("parsing file data");
-      console.log(fileData.toString());
-   });
+   // fs.readFile("data.txt", function (error, fileData) {
+   //    console.log("parsing file data");
+   //    console.log(fileData.toString());
+   // });
+   fs.readFile("data.txt")
+      .then((fileData) => {
+         console.log("First asyncho code");
+         console.log(fileData.toString());
+      })
+      .then(() => {
+         console.log("second asyncho code");
+      });
 
    console.log("hi lmao");
 }
