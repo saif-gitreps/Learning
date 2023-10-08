3d9605e9a327adfd60dabc6a371ae7e6b322087a
Saif <droconos99@gmail.com>
2023-10-08 12:02:24 +0600

    using async and await

--- web_dev_study/udemy-section-21/async.js
+++ web_dev_study/udemy-section-21/async.js
@@ -1,23 +1,20 @@
 // one way-> const fs = require("fs");
 const fs = require("fs/promises");
 
-function readFile() {
+async function readFile() {
    let fileData;
    // fs.readFile("data.txt", function (error, fileData) {
    //    console.log("parsing file data");
    //    console.log(fileData.toString());
    // });
-   fs.readFile("data.txt")
-      .then((fileData) => {
-         console.log("First asyncho code");
-         console.log(fileData.toString());
-      })
-      .then(() => {
-         console.log("second asyncho code");
-      })
-      .catch((error) => {
-         console.log(error);
-      });
+   try {
+      fileData = await fs.readFile("data.txt");
+   } catch (error) {
+      console.log(error);
+   }
+   console.log("First asyncho code");
+   console.log(fileData.toString());
+   console.log("second asyncho code");
    console.log("hi lmao");
 }
 readFile();
