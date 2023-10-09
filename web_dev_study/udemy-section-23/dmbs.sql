74884947a3a270ad84363c8f8be368ead50195b7
Saif <droconos99@gmail.com>
2023-10-09 19:00:31 +0600

    creating table with conditions

--- web_dev_study/udemy-section-23/dmbs.sql
+++ web_dev_study/udemy-section-23/dmbs.sql
@@ -1,1 +1,10 @@
+/* To create database */
 create database restaurant_finder;
+
+/* To create a table with conditions, backticks are used when using special characters. */
+CREATE TABLE restaurant_finder.restaurants (
+  id INT NOT NULL,
+  name VARCHAR(200) NOT NULL,
+  type VARCHAR(200) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);
