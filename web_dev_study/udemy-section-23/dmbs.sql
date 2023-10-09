1f1eaa5f5a5bc90bfa62bdeb23896776ad72866b
Saif <droconos99@gmail.com>
2023-10-09 19:11:14 +0600

    adding new rows

--- web_dev_study/udemy-section-23/dmbs.sql
+++ web_dev_study/udemy-section-23/dmbs.sql
@@ -8,3 +8,8 @@ CREATE TABLE restaurant_finder.restaurants (
   type VARCHAR(200) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);
+
+=>inserting. 
+
+INSERT INTO restaurants(id ,name, type) VALUES(1,'Morroccan Res' , 'Morrocan')
+INSERT INTO restaurants(id, name, type) VALUES(2,'Italiano Res' , 'Italian');
