4c7795726d46bd18883795e9df4c6fd9e204c584
Saif <droconos99@gmail.com>
2023-11-01 19:05:00 +0600

    added conversion of lower to upper in asm

---
+++ Learning_notes/Assembly-lang/Lower-to-UpperCase.asm
@@ -0,0 +1,30 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+main proc
+
+MOV AH,1
+INT 21H
+MOV BL,AL
+INT 21H
+MOV CL,AL
+
+ADD BL,32
+ADD CL,32        
+        
+MOV AH,2
+MOV DL,32
+INT 21H
+MOV DL,BL
+INT 21H
+MOV DL,CL
+INT 21H
+
+ret
+
+
+
+
