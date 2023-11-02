1e0597dc934258e80b8fc426cb1e46c8f3d40eb0
Saif <droconos99@gmail.com>
2023-11-02 21:07:03 +0600

    updating one and many in MDB

--- web_dev_study/udemy-section-25/CRUD-operations.js
+++ web_dev_study/udemy-section-25/CRUD-operations.js
@@ -3,3 +3,15 @@ db.books.deleteOne({ _id: ObjectId("6543983b77bbad8150aa5854") });
 
 //deleting many documents with conditions:
 db.books.deleteMany({ author: "JKR" });
+
+//Updating a single doc:
+db.books.updateOne(
+   { _id: ObjectId("6543983b77bbad8150aa5854") },
+   { $set: { rating: 8, pages: 461 } }
+);
+// here first arg is the matching condition
+// second is all the values we set and update. we can have multiple properties updated using a coma.
+
+//To update mutiple :
+db.books.updateMany({ author: "JKR" }, { $set: { title: "Happy potter infinity" } });
+//bscially translates to update set title = "new titile" where author is "JKR".
