4a44f3ae03414d7d153e6446f92db44e24c9e8bb
Saif <droconos99@gmail.com>
2023-11-04 14:17:23 +0600

    added more assembly notes

---
+++ Learning_notes/Assembly-lang/Check-two-num-equal-or-no.asm
@@ -0,0 +1,40 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+.DATA
+    A DB 'EQUAL$'
+    B DB 'NOT EQUAL$'         
+.code
+main proc
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV AH,1
+    INT 21H
+    MOV BL,AL
+    INT 21H
+    MOV CL,AL
+    
+    CMP BL,CL
+    JE L1
+    JNE L2
+    
+    L1:
+    LEA DX,A
+    MOV AH,9
+    INT 21H
+    JMP
+    
+    L2:
+    LEA DX,B
+    MOV AH,9
+    INT 21H
+    JMP
+
+ret
+
+
+
+
