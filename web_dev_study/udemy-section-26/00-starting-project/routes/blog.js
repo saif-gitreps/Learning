95f5153cb0ef64f86d58ac87b2a21a78feae28cb
Saif <droconos99@gmail.com>
2023-12-02 23:27:45 +0600

    adding post/insert using mongodb

--- web_dev_study/udemy-section-26/00-starting-project/routes/blog.js
+++ web_dev_study/udemy-section-26/00-starting-project/routes/blog.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const db = require("../data/database");
+const mongodb = require("mongodb");
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -14,6 +17,22 @@ router.get("/posts", function (req, res) {
 router.get("/new-post", async function (req, res) {
    const authors = await db.getDb().collection("authors").find().toArray();
    res.render("create-post", { authors: authors });
+});
+
+router.post("/posts", async (request, response) => {
+   const author_id = new ObjectId(request.body.author);
+   const author_name = await db.getDb.collection("authors").findOne({ _id: author_id });
+
+   const newPost = {
+      title: request.body.title,
+      summary: request.body.summary,
+      body: request.body.content,
+      date: new Date(),
+      author: {
+         id: author_id,
+         name: author_name,
+      },
+   };
 });
 
 module.exports = router;
