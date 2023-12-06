5c34f8ae3a860ef1a5a1cfb340759964a47a3f17
Saif <droconos99@gmail.com>
2023-12-07 00:35:23 +0600

    Added multer for file uploading

--- web_dev_study/udemy-section-27/00-starting-project/routes/users.js
+++ web_dev_study/udemy-section-27/00-starting-project/routes/users.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
+
+const upload = multer({});
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get("/", function (req, res) {
+   res.render("profiles");
 });
 
-router.get('/new-user', function(req, res) {
-  res.render('new-user');
+router.get("/new-user", function (req, res) {
+   res.render("new-user");
+});
+
+router.post("/profiles", upload.single("image"), (req, res) => {
+   const uploadedImage = req.file;
+   const userData = req.body;
 });
 
 module.exports = router;
