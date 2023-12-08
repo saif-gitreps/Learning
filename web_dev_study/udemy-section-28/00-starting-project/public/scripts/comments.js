d30acd050a99f2ec23202315a44a1aa00b8624a9
Saif <droconos99@gmail.com>
2023-12-08 22:41:26 +0600

    added ajax route for loading comments

--- web_dev_study/udemy-section-28/00-starting-project/public/scripts/comments.js
+++ web_dev_study/udemy-section-28/00-starting-project/public/scripts/comments.js
@@ -1,1 +1,8 @@
 const loadCommentsButton = document.getElementById("load-comments-btn");
+
+loadCommentsButton.addEventListener("click", async (event) => {
+   const postId = loadCommentsButton.dataset.postid;
+   const response = await fetch(`/posts/${postId}/comments`);
+   const responseData = await response.json();
+   console.log(responseData);
+});
