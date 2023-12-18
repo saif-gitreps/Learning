096593cd8532e14fcbf6cdb24ed609a8ce363c09
Saif <droconos99@gmail.com>
2023-12-18 13:23:10 +0600

    Added error handlers in broswer side Js

--- web_dev_study/udemy-section-28/00-starting-project/public/scripts/comments.js
+++ web_dev_study/udemy-section-28/00-starting-project/public/scripts/comments.js
@@ -1,3 +1,5 @@
+const { reset } = require("nodemon");
+
 const loadCommentsButton = document.getElementById("load-comments-btn");
 const commentSection = document.getElementById("comments");
 const commentForm = document.querySelector("#comments-form form");
@@ -21,18 +23,28 @@ function createSectionList(comments) {
 
 async function loadCommentsCallback(event) {
    const postId = loadCommentsButton.dataset.postid;
-   const response = await fetch(`/posts/${postId}/comments`);
-   // we convert the response to json
-   const responseData = await response.json();
+   try {
+      const response = await fetch(`/posts/${postId}/comments`);
 
-   if (responseData && responseData.length > 0) {
-      // we create a new list of comments
-      const newCommentList = createSectionList(responseData);
-      // we replace the old list with the new one
-      commentSection.innerHTML = "";
-      commentSection.appendChild(newCommentList);
-   } else {
-      commentSection.firstElementChild.textContent = "No comments yet!";
+      if (!response.ok) {
+         alert("something went wrong while loading comments");
+         return;
+      }
+
+      // we convert the response to json
+      const responseData = await response.json();
+
+      if (responseData && responseData.length > 0) {
+         // we create a new list of comments
+         const newCommentList = createSectionList(responseData);
+         // we replace the old list with the new one
+         commentSection.innerHTML = "";
+         commentSection.appendChild(newCommentList);
+      } else {
+         commentSection.firstElementChild.textContent = "No comments yet!";
+      }
+   } catch (error) {
+      alert("Something went wrong on the technical side!");
    }
 }
 
@@ -49,15 +61,22 @@ commentForm.addEventListener("submit", async (event) => {
       title: enteredTitle,
       text: enteredText,
    };
+   try {
+      const response = await fetch(`/posts/${postId}/comments`, {
+         method: "POST",
+         body: JSON.stringify(actualComment),
+         headers: {
+            "Content-Type": "application/json",
+         },
+      });
 
-   const response = await fetch(`/posts/${postId}/comments`, {
-      method: "POST",
-      body: JSON.stringify(actualComment),
-      headers: {
-         "Content-Type": "application/json",
-      },
-   });
-
-   // so after we added the comment , we reload the comments
-   loadCommentsCallback();
+      if (response.ok) {
+         // so after we added the comment , we reload the comments
+         loadCommentsCallback();
+      } else {
+         alert("Something went wrong on the server!");
+      }
+   } catch (error) {
+      alert("Something went wrong on the technical side!");
+   }
 });
