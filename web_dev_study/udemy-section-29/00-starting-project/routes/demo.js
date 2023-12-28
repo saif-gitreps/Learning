f91645623090442532cfff612a3e5bc374c0241d
Saif <droconos99@gmail.com>
2023-12-28 21:01:11 +0600

    added validating password cred

--- web_dev_study/udemy-section-29/00-starting-project/routes/demo.js
+++ web_dev_study/udemy-section-29/00-starting-project/routes/demo.js
@@ -33,7 +33,41 @@ router.post("/signup", async function (req, res) {
    res.redirect("/login");
 });
 
-router.post("/login", async function (req, res) {});
+router.post("/login", async function (req, res) {
+   const userData = req.body;
+   const enteredEmail = userData.email;
+   const enteredPassword = userData.password;
+
+   const existingUser = await db
+      .getDb()
+      .collection("users")
+      .findOne({ email: enteredEmail });
+
+   if (!existingUser) {
+      console.log("User does not exist");
+      return res.redirect("/login");
+   }
+   // this is another way of doing async await.
+   // bcrypt.compare(enteredPassword, existingUser.password).then(function (result) {
+   //    if (result) {
+   //       console.log("User logged in");
+   //       res.redirect("/admin");
+   //    } else {
+   //       console.log("password do not match");
+   //       res.redirect("/login");
+   //    }
+   // });
+
+   const passwordCheck = await bcrypt.compare(enteredPassword, existingUser.password);
+
+   if (!passwordCheck) {
+      console.log("password do not match");
+      return res.redirect("/login");
+   }
+
+   console.log("User logged in");
+   res.redirect("/admin");
+});
 
 router.get("/admin", function (req, res) {
    res.render("admin");
