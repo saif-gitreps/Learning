a863586460f4807c698db45ba023cc626fe1ac1b
Saif <droconos99@gmail.com>
2023-12-31 13:33:28 +0600

    added middleware res.locals for all ejs template

--- web_dev_study/udemy-section-29/00-starting-project/app.js
+++ web_dev_study/udemy-section-29/00-starting-project/app.js
@@ -35,6 +35,21 @@ app.use(
    })
 );
 
+app.use(async function (req, res, next) {
+   const user = req.session.user;
+   const isAuth = req.session.isAuthenticated;
+
+   if (!user || !isAuth) {
+      return next();
+   }
+
+   const userFromDatabase = db.getDb().collection("users").findOne({ _id: user.id });
+   const isAdmin = userFromDatabase.isAdmin;
+
+   res.locals.isAuth = isAuth;
+   res.locals.isAdmin = isAdmin;
+});
+
 app.use(demoRoutes);
 
 app.use(function (error, req, res, next) {
