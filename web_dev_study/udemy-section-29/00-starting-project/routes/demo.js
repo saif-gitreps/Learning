9d589153fbd3e3050ed45ebc21646bbef0680c4c
Saif <droconos99@gmail.com>
2023-12-31 12:48:49 +0600

    added an existing user feature

--- web_dev_study/udemy-section-29/00-starting-project/routes/demo.js
+++ web_dev_study/udemy-section-29/00-starting-project/routes/demo.js
@@ -65,8 +65,17 @@ router.post("/signup", async function (req, res) {
       .findOne({ email: enteredEmail });
 
    if (existingUser) {
-      console.log("User already exists");
-      return res.redirect("/signup");
+      req.session.inputData = {
+         hasError: true,
+         message: "User exists already!",
+         email: enteredEmail,
+         confirmEmail: enteredConfirmEmail,
+         password: enteredPassword,
+      };
+      req.session.save(function () {
+         res.redirect("/signup");
+      });
+      return;
    }
 
    const user = {
