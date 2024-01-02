6857359b65b851126c7ea48b3e4b483b5bb1ddc2
Saif <droconos99@gmail.com>
2024-01-02 20:34:24 +0600

    section 30 ended

---
+++ web_dev_study/udemy-section-30/00-starting-projects/db-injection/data/database.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+   host: "localhost",
+   database: "blog",
+   user: "root",
+   password: "CSE123",
+   multipleStatements: true,
+});
+
+module.exports = pool;
