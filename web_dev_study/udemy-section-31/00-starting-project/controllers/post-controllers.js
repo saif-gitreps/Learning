d7bd0b9207b25fac3555df2773a88296264fe494
Saif <droconos99@gmail.com>
2024-01-04 20:23:22 +0600

    refactored session data, didnt completely do though

--- web_dev_study/udemy-section-31/00-starting-project/controllers/post-controllers.js
+++ web_dev_study/udemy-section-31/00-starting-project/controllers/post-controllers.js
@@ -1,4 +1,6 @@
 const Post = require("../models/post");
+const validationSession = require("../util/validation-session");
+const sessionValidation = require("../util/validation-session");
 
 function getHome(req, res) {
    res.render("welcome", { csrfToken: req.csrfToken() });
@@ -11,21 +13,14 @@ async function getAdmin(req, res) {
 
    const posts = await Post.fetchAll();
 
-   let sessionInputData = req.session.inputData;
-
-   if (!sessionInputData) {
-      sessionInputData = {
-         hasError: false,
-         title: "",
-         content: "",
-      };
-   }
-
-   req.session.inputData = null;
+   sessionErrorData = sessionValidation.getSessionErrorData(req, {
+      title: "",
+      content: "",
+   });
 
    res.render("admin", {
       posts: posts,
-      inputData: sessionInputData,
+      inputData: sessionErrorData,
       csrfToken: req.csrfToken(),
    });
 }
@@ -40,15 +35,18 @@ async function createPost(req, res) {
       enteredTitle.trim() === "" ||
       enteredContent.trim() === ""
    ) {
-      req.session.inputData = {
-         hasError: true,
-         message: "Invalid input - please check your data.",
-         title: enteredTitle,
-         content: enteredContent,
-      };
-
-      res.redirect("/admin");
-      return; // or return res.redirect('/admin'); => Has the same effect
+      validationSession.flashErrorSession(
+         req,
+         {
+            message: "Invalid input - please check your data.",
+            title: enteredTitle,
+            content: enteredContent,
+         },
+         function () {
+            res.redirect("/admin");
+         }
+      );
+      return;
    }
 
    const newPost = new Post(enteredTitle, enteredContent);
@@ -67,21 +65,14 @@ async function getSinglePost(req, res) {
       return res.render("404"); // 404.ejs is missing at this point - it will be added later!
    }
 
-   let sessionInputData = req.session.inputData;
-
-   if (!sessionInputData) {
-      sessionInputData = {
-         hasError: false,
-         title: post.title,
-         content: post.content,
-      };
-   }
-
-   req.session.inputData = null;
+   sessionErrorData = sessionValidation.getSessionErrorData(req, {
+      title: post.title,
+      content: post.content,
+   });
 
    res.render("single-post", {
       post: post,
-      inputData: sessionInputData,
+      inputData: sessionErrorData,
       csrfToken: req.csrfToken(),
    });
 }
