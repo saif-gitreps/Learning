06532d1edc303ddab0387d76ef1cbd2bc8711c72
Saif <droconos99@gmail.com>
2024-01-18 12:35:48 +0600

    full-stack-mern aspp practice

---
+++ Full-Stack-Practice/backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+   res.send("hi");
+});
+
+app.get("/jokes", (req, res) => {
+   const jokes = [
+      {
+         id: 1,
+         title: "A Joke",
+         contents: "first joke",
+      },
+      {
+         id: 2,
+         title: "Another Joke",
+         content: "second joke",
+      },
+      {
+         id: 3,
+         title: "Last Joke",
+         content: "final joke",
+      },
+   ];
+   res.send(jokes);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log("server ready at " + PORT);
+});
