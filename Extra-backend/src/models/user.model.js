b3aa07017689bb0aff35c1bf4feb5b78c1668c8e
Saif <droconos99@gmail.com>
2024-01-21 22:44:42 +0600

    Adding hooks for mongoose like pre (type of middleware)

--- Extra-backend/src/models/user.model.js
+++ Extra-backend/src/models/user.model.js
@@ -1,4 +1,7 @@
 const mongoose = require("moongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
 const userSchema = new mongoose.Schema(
    {
       username: {
@@ -49,6 +52,19 @@ const userSchema = new mongoose.Schema(
    },
    { timestamps: true }
 );
+// remember arrow function cannot keep reference, tats why we use the traditional method.
+// this is a middle that performs something when a query might be runnig, like if save method were
+// to run then we would so something.
+// now another problem that might arise with this code is that everytime a user updates anything else,
+// this password will get encrypted again no matter what. so we have to add some conditions.
+// we only want this encyrption to happen only during the frist time or if the user has updated their password.
+userSchema.pre("save", async function (next) {
+   if (!this.isModified("password")) {
+      return next();
+   }
+   this.password = await bcrypt.hash(this.password, 12);
+   next();
+});
 
 const User = mongoose.model("User", userSchema);
 
