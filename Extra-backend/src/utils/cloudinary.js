6625d6a8cbe099da516471e67935d8dd4939e9bb
Saif <droconos99@gmail.com>
2024-01-22 11:32:00 +0600

    Added cloudinary config and env variables.

---
+++ Extra-backend/src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+   try {
+      if (!localFilePath) {
+         return null;
+      }
+      // next step upload the file on cloudinary.
+      const response = await cloudinary.uploader.upload(localFilePath, {
+         // could be anything like img,vid, so put auto.
+         resource_type: "auto",
+      });
+      console.log("File uploaded on cloudinary: " + response.url);
+      return response;
+   } catch (error) {
+      // fs has this unlink option. this will remove the locally saved temporary file
+      // as the upload operation got failed.
+      fs.unlinkSync(localFilePath);
+      return null;
+   }
+};
+
+module.exports = uploadOnCloudinary;
