ab81242e1a20238d7718f7408a7d6a260fda3543
Saif <droconos99@gmail.com>
2024-01-26 19:14:49 +0600

    Added subscription model

--- Extra-backend/src/middlewares/auth.middleware.js
+++ Extra-backend/src/middlewares/auth.middleware.js
@@ -44,4 +44,24 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
    }
 });
 
+// const verifyJWT = asyncHandler(async (req, res, next) => {
+//    const token =
+//       req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+//    if (!token) {
+//       throw new ApiError(401, "Unauthorized Req");
+//    }
+
+//    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+//    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+
+//    if (!user) {
+//       throw new ApiError(401, "Invalid access credentials");
+//    }
+
+//    req.user = user;
+//    next();
+// });
+
 module.exports = verifyJWT;
